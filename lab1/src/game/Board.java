90ba0b3071876ad9b7b74cafdc63ea0ccd06a4bf
'Kristjan' <'pe1178jo-s@student.lu.se'>
2022-09-07 17:22:51 +0200

    Fixed game setup with 0 sticks

--- lab1/src/game/Board.java
+++ lab1/src/game/Board.java
@@ -5,6 +5,10 @@ public class Board {
 	
 	
 	public void setUp(int n) {
+		while (n < 4) {
+			JOptionPane.showMessageDialog(null, "Du behöver minst 4 stickor för att spela", "Error", JOptionPane.ERROR_MESSAGE);
+			n = UserInterface.askForInt(JOptionPane.showInputDialog("Hur många stickor?"), 2);
+		}
 		noPins = n;
 	}
 	
