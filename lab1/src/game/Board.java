c8ff62019401ef6605b21b8070efc8aa454ff553
'Kristjan' <'pe1178jo-s@student.lu.se'>
2022-09-07 17:26:41 +0200

    Id 2 -> id 1 setUp

--- lab1/src/game/Board.java
+++ lab1/src/game/Board.java
@@ -9,7 +9,7 @@ public class Board {
 	public void setUp(int n) {
 		while (n < 4) {
 			JOptionPane.showMessageDialog(null, "Du behöver minst 4 stickor för att spela", "Error", JOptionPane.ERROR_MESSAGE);
-			n = UserInterface.askForInt(JOptionPane.showInputDialog("Hur många stickor?"), 2);
+			n = UserInterface.askForInt(JOptionPane.showInputDialog("Hur många stickor?"), 1);
 		}
 		noPins = n;
 	}
