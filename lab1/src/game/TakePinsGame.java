1b532ee047989ecb1998f96f263e9239876ad2fd
Kristjan <pe1178jo-s@student.lu.se>
2022-09-21 21:52:47 +0200

    Fix redundancy & naming cp

--- lab1/src/game/TakePinsGame.java
+++ lab1/src/game/TakePinsGame.java
@@ -7,60 +7,38 @@ public class TakePinsGame {
 
 	public static void main(String[] args) {
 		Board b = new Board();
-
+		Player cp = null;
 		String[] options = {"ComputerPlayer", "ModuloComputerPlayer"};
-		int x = JOptionPane.showOptionDialog(null, "Välj din motspelare",
-		"Välj",
-		JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
-		HumanPlayer hp = new HumanPlayer(JOptionPane.showInputDialog("Ditt namn:")); 
-		
-		if (x == 0) {
-			ComputerPlayer cp = new ComputerPlayer(JOptionPane.showInputDialog("Computerns namn:"));
+		HumanPlayer hp = new HumanPlayer(JOptionPane.showInputDialog("Ditt namn:"));
+		String cpname = JOptionPane.showInputDialog("Computerns namn:");
 
-			b.setUp(UserInterface.askForInt(JOptionPane.showInputDialog("Hur många stickor vill du spela med?"), 1));
-			UserInterface.printMessage(hp.userId + " vs. " + cp.userId + "\nSpelet börjar med " + Integer.toString(b.getNoPins()) + " stickor på bordet");
-	
-			while (b.getNoPins() > 0) {
-				
-				if (b.getNoPins() > 0) {
-					UserInterface.printMessage(hp.getUserId() + " tog " + hp.takePins(b));
-					if (b.getNoPins() == 0) {
-						UserInterface.printMessage(hp.getUserId() + " vann");
-					}
-				}
-				
-				if (b.getNoPins() > 0) {
-					UserInterface.printMessage(cp.getUserId() + " tog " + cp.takePins(b));
-					if (b.getNoPins() == 0) {
-						UserInterface.printMessage(cp.getUserId() + " vann");
-					}
-				}
-			}
-
+		if (JOptionPane.showOptionDialog(null, "Välj din motspelare", "Välj", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]) < 1) {
+			cp = new ComputerPlayer(cpname);
 		}
-		else if (x == 1) {
+		else {
+			cp = new ModComputerPlayer(cpname);
+		}
+		
+		b.setUp(UserInterface.askForInt(JOptionPane.showInputDialog("Hur många stickor vill du spela med?"), 1));
+		UserInterface.printMessage(hp.userId + " vs. " + cp.userId + "\nSpelet börjar med " + Integer.toString(b.getNoPins()) + " stickor på bordet");
 
-			ModComputerPlayer cp = new ModComputerPlayer(JOptionPane.showInputDialog("Computerns namn:"));
-
-			b.setUp(UserInterface.askForInt(JOptionPane.showInputDialog("Hur många stickor vill du spela med?"), 1));
-			UserInterface.printMessage(hp.userId + " vs. " + cp.userId + "\nSpelet börjar med " + Integer.toString(b.getNoPins()) + " stickor på bordet");
-	
-			while (b.getNoPins() > 0) {
-				
-				if (b.getNoPins() > 0) {
-					UserInterface.printMessage(hp.getUserId() + " tog " + hp.takePins(b));
-					if (b.getNoPins() == 0) {
-						UserInterface.printMessage(hp.getUserId() + " vann");
-					}
-				}
-				
-				if (b.getNoPins() > 0) {
-					UserInterface.printMessage(cp.getUserId() + " tog " + cp.takePins(b));
-					if (b.getNoPins() == 0) {
-						UserInterface.printMessage(cp.getUserId() + " vann");
-					}
+		while (b.getNoPins() > 0) {
+			
+			if (b.getNoPins() > 0) {
+				UserInterface.printMessage(hp.getUserId() + " tog " + hp.takePins(b));
+				if (b.getNoPins() == 0) {
+					UserInterface.printMessage(hp.getUserId() + " vann");
 				}
 			}
+			
+			if (b.getNoPins() > 0) {
+				UserInterface.printMessage(cp.getUserId() + " tog " + cp.takePins(b));
+				if (b.getNoPins() == 0) {
+					UserInterface.printMessage(cp.getUserId() + " vann");
+				}
+			}
+		}
+
 
 
 		}
@@ -72,4 +50,3 @@ public class TakePinsGame {
 
 	}
 
-}
